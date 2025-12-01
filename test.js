de5339e3758a0146b4832a864c984308a7c62770
jihun <teachingmeaning1208@gmail.com>
2025-12-01 22:21:44 +0900

    extends & super

---
+++ test.js
@@ -0,0 +1,33 @@
+class Car{
+    constructor(color){
+        this.color = color;
+        this.wheels = 4;
+    }
+    drive(){
+        console.log("drive..");
+    }
+    stop(){
+        console.log("STOP!");
+    }
+}
+
+class Bmw extends Car{
+    park(){
+        console.log("PARK");
+    }
+    stop(){
+        super.stop();
+        console.log("and.. wait!");
+    }
+}
+
+class Newbmw extends Bmw{
+    stop(){
+        super.stop();
+        console.log("yre!!");
+    }
+}
+
+const mybmw = new Newbmw("white");
+
+sexybmw.stop();
