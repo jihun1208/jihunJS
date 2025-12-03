aae6b160730e3c0d9b0c4ca55d4ce0071f9cee45
jihun <teachingmeaning1208@gmail.com>
2025-12-03 16:33:03 +0900

    input값 받아 출력하기

--- test.js
+++ test.js
@@ -1,41 +1,4 @@
-const f1 = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("1번 주문 완료");
-        }, 1000);
-    });
+function inputting(){
+    const cont = document.getElementById("ip").value;
+    document.getElementById("op").innerHTML = cont;
 }
-
-const f2 = (message) => {
-    console.log(message);
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("2번 주문 완료");
-        }, 3000);
-    });
-}
-
-const f3 = (message) => {
-    console.log(message);
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("3번 주문 완료");
-        }, 2000);
-    });
-}
-
-async function order(){
-    const result1 = await f1();
-    const result2 = await f2(result1);
-    const result3 = await f3(result2);
-    console.log(result3);
-    console.log("끝");
-}
-
-order();
-
-// f1().then((result) => f2(result))
-// .then((result) => f3(result))
-// .then((result) => console.log(result))
-// .catch()
-// .finally(() => {console.log("끝");});
